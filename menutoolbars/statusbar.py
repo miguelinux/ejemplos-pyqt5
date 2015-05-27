b4472f2f481dd26944fd29ddd3da8b043bac3ac3
Miguel Bernal Marin <mbernal@itszapopan.edu.mx>
2015-05-26 22:07:52 -0500

    Agregar statusbar
    Signed-off-by: Miguel Bernal Marin <mbernal@itszapopan.edu.mx>

---
+++ menutoolbars/statusbar.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode PyQt5 tutorial
+
+This program creates a statusbar.
+
+author: Jan Bodnar
+website: zetcode.com
+last edited: January 2015
+"""
+
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+
+class Example(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        self.statusBar().showMessage('Ready')
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Statusbar')
+        self.show()
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
